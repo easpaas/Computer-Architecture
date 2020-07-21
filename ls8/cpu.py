1e30d26903dd2e08dceff15e451cc3932466248f
evan <aspaasevan@gmail.com>
2020-07-21 07:00:13 -0700

    cpu constructor complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,31 +7,49 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        # self.ram = 
-        # self.pc
-        # self.reg
+
+        # register holds storage via a list
+        self.reg = [0] * 8
+        self.pc =  0
+        self.ram = [0] * 256
+        self.running = False
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.HLT = 0b00000001
 
     def load(self):
         """Load a program into memory."""
 
-        # At address 0...
+        # At address 0...(memory controller accesses RAM via address)
         address = 0
 
-        # For now, we've just hardcoded a program:
-
+        # For now, we've just hardcoded a program: 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            0b10000010, # LDI R0,8 -> call LDI with params R0 and 8 (ADDRESS: 0)
+            0b00000000,     # R0    (ADDRESS: 1)
+            0b00001000,     # binary value of 8  (ADDRESS: 2)
+            0b01000111, # PRN R0 -> call PRN with param R0 (ADDRESS: 3)
+            0b00000000,     # R0 (ADDRESS: 4)
+            0b00000001, # HLT -> Halt (ADDRESS: 5)
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+
+            # if instruction == 0b10000010:  # -- > LDI R0, 8
+                # 
+                # increase address pointer by 1 
+                # address += 1
+                
+
+
+            # if instruction == 0b01000111:
+                # increase address pointer by 1 
+                # address += 1
+                # print value of R0
+                # print(f"instruction: {instruction}")
 
 
     def alu(self, op, reg_a, reg_b):
@@ -61,8 +79,10 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-        print()
-
     def run(self):
         """Run the CPU."""
+        # TODO
+            # 
+            #  
+            #
         pass
