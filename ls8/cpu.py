ba6d0ca6f135cb5b382c7a0a0620c0097a77a7e1
evan <aspaasevan@gmail.com>
2020-07-24 07:42:06 -0700

    refactor cpu constructor into individual methods, moved dispatch table to run()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,66 +7,73 @@ class CPU:
 
         # register holds 8 bytes
         self.reg = [0] * 8
+        # 256 bytes of memory
         self.ram = [0] * 256
         # Program Counter, index into ram 
         self.pc =  0
-        self.running = False
-        #  Add binary
-        self.ADD = 10100000
-        # Load binary
-        self.LDI = 10000010
-        # Print binary
-        self.PRN = 0b01000111
-        # Halt binary
-        self.HLT = 0b00000001
-        # Multiply binary
-        self.MUL = 10100010
-        # PUSH binary
-        self.PUSH = 0b01000101
-        # POP binary
-        self.POP = 0b01000110
-        # Stack pointer starts at F4
-        self.SP = 0xf4
-        # CALL binary
-        self.CALL = 0b01010000
-        # RET binary
-        self.RET = 0b00010001
-        # Dispatch Table
-        self.configure_dispatch_table()
 
-    def configure_dispatch_table(self):
-        self.dispatch_table = {}
-
-        self.dispatch_table[self.LDI] = self.ldi
-        self.dispatch_table[self.PRN] = self.prn
-        self.dispatch_table[self.HLT] = self.hlt
-        self.dispatch_table[self.PUSH] = self.push
-        self.dispatch_table[self.POP] = self.pop
-        
     #  accept the address to read and return the value stored there
-    def ram_read(self, index):
-        return self.ram[index]
+    def ram_read(self, address):
+        return self.ram[address]
 
     # accept a value to write, and the address to write it to 
-    def ram_write(self, index, value):
-        self.ram[index] = value
+    def ram_write(self, address, value):
+        self.ram[address] = value
 
-    def hlt(self):
-        sys.exit()
-
-    def ldi(self, a, value):
+    # Load a value in register
+    def LDI(self, a, value):
         self.reg[a] = value
 
-    def pop(self, a):
-        self.reg[a] = self.ram_read(self.SP)
-        self.SP += 1
-
-    def prn(self, a):
+    # Print value at register index  
+    def PRN(self, a):
         print(self.reg[a])
 
-    def push(self, a):
-        self.SP -= 1
-        self.ram_write(self.SP, self.reg[a])
+    # Halt the CPU
+    def HLT(self):
+        sys.exit()
+
+    def POP(self, a):
+        addr_to_pop_from = self.reg[7]
+        value = self.ram_read(addr_to_pop_from)
+
+        reg_num = self.ram_read(self.pc + 1)
+        self.reg[reg_num] = value
+        
+        self.reg[7] += 1
+
+    def PUSH(self, reg):
+        self.reg[7] -= 1
+
+        reg_num = self.ram_read(self.pc + 1)
+        value = self.reg[reg_num]
+
+        addr_to_push_to = self.reg[7]
+        self.ram_write(value, addr_to_push_to)
+
+    def MUL(self, a, b):
+        self.alu('MUL', a, b)
+    
+    def ADD(self, regA, regB):
+        self.alu('ADD', regA, regB)
+
+    def CALL(self, reg): 
+        return_addr = self.pc + 2
+
+        self.reg[7] -= 1
+        addr_to_push_to = self.reg[7]
+        self.ram_write(return_addr,addr_to_push_to)
+
+        reg_num = self.ram_read(self.pc + 1)
+        subroutine_addr = self.reg[reg_num]
+
+        self.pc = subroutine_addr
+
+    def RET(self):
+        addr_to_pop_from = self.reg[7]
+        return_addr = self.ram_read(addr_to_pop_from)
+        self.reg[7] += 1
+
+        self.pc = return_addr
 
     def load(self):
         """Load a program into memory."""
@@ -82,7 +89,7 @@ class CPU:
                 for line in filename:
                     try:
                         line = line.split("#",1)[0]
-                        line = int(line, 10)  # int() is base 10 by default
+                        line = int(line, 2)  # int() is base 10 by default
                         self.ram[address] = line
                         address += 1
                     except ValueError:
@@ -93,10 +100,10 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == self.ADD:
+        if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
-        elif op == self.MUL:
+        elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
         else: 
             raise Exception("Unsupported ALU operation")
@@ -122,82 +129,48 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        self.running = True
+        running = True
+
+        dispatch_table = {
+            '0b10000010': self.LDI,
+            '0b01000111': self.PRN,
+            '0b00000001': self.HLT,
+            '0b01000101': self.PUSH,
+            '0b01000110': self.POP,
+            '0b10100010': self.MUL,
+            '0b10100000': self.ADD,
+            '0b01010000': self.CALL, 
+            '0b00010001': self.RET
+        }
         
-        while self.running:
-            # IR instruction register, equal to current address in memory
-            ir = self.ram[self.pc]
-            print(f'current instruction: {ir}')
+        while running:
+            ir = self.ram_read(self.pc)
+            # operand 1
+            op1 = self.ram_read(self.pc+1)
+            # operand 2
+            op2 = self.ram_read(self.pc+2)
 
-            if ir == self.LDI:  
-                reg_num = self.ram_read(self.pc+1)
-                value = self.ram_read(self.pc+2)
-                self.reg[reg_num] = value
-                self.pc += 3
+            binStr = format(ir, '#010b')
+            args = binStr[2:4]
+            alu_op = binStr[4]
+            set_pc = binStr[5]
+            inst_id = binStr[6:]
 
-            elif ir == self.PRN:
-                reg_num = self.ram[self.pc+1]
-                print(self.reg[reg_num])
-                self.pc += 2
+            if binStr in dispatch_table:
+                function = dispatch_table[binStr]
+                if args == "00": 
+                    function()
+                    if set_pc == '0': 
+                        self.pc += 1
+                elif args == "01": 
+                    function(op1)
+                    if set_pc == '0': 
+                        self.pc += 2
+                elif args == "10": 
+                    function(op1, op2)
+                    if set_pc == '0':
+                        self.pc += 3
 
-            elif ir == self.HLT:
-                self.running = False
-                self.pc += 1
-
-            elif ir == self.PUSH: 
-                # decrement stack pointer
-                self.reg[self.SP] -= 1
-                self.reg[self.SP] &= 0xff  # keep R7 in the range 00-FF
-
-                # get register value
-                reg_num = self.ram[self.pc + 1]
-                value = self.reg[reg_num]
-
-                # Store in memory
-                address_to_push_to = self.reg[self.SP]
-                self.ram[address_to_push_to] = value
-                
-                self.pc += 2
-
-            elif ir == self.POP:  
-                # Get value from RAM
-                address_to_pop_from = self.reg[self.SP]
-                value = self.ram[address_to_pop_from]
-
-                # Store in the given register
-                reg_num = self.ram[self.pc + 1]
-                self.reg[reg_num] = value
-
-                # Increment SP
-                self.reg[self.SP] += 1
-
-                self.pc += 2
-
-            elif ir == self.CALL:
-                # Get address of the next instruction
-                return_addr = pc + 2
-
-                # Push that on the stack
-                self.reg[self.SP] -= 1
-                address_to_push_to = self.reg[self.SP]
-                self.ram[address_to_push_to] = return_addr
-
-                # Set the PC to the subroutine address
-                reg_num = self.ram[pc + 1]
-                subroutine_addr = self.reg[reg_num]
-
-                pc = subroutine_addr
-
-            elif ir == self.RET:
-                # Get return address from the top of the stack
-                address_to_pop_from = self.reg[self.SP]
-                return_addr = self.ram[address_to_pop_from]
-                self.reg[self.SP] += 1
-
-                # Set the PC to the return address
-                pc = return_addr
-
-
-            else:
-                print(f"Did not understand that instruction: {ir} at address {self.pc}")
-                sys.exit(1)
+            # else:
+            #     print(f"Did not understand that instruction: {ir} at address {self.pc}")
+            #     sys.exit(1)
