87170cc74629d3e277f34c9e3ba5a2da7dd76bed
evan <aspaasevan@gmail.com>
2020-07-24 09:03:03 -0700

    cmp, jmp jne and jeq methods complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,111 +1,177 @@
-"""CPU functionality."""
 import sys
 
+# BINARY values
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+CALL = 0b01010000  # 00000rrr
+RET = 0b00010001
+ADD = 0b10100000  # 00000aaa 00000bbb
+CMP = 0b10100111
+JMP = 0b01010100
+JNE = 0b01010110
+JEQ = 0b01010101
+
+
 class CPU:
+    """Main CPU class."""
+
     def __init__(self):
         """Construct a new CPU."""
+        self.ram = [0] * 256   # 256 bytes of memory
+        self.reg = [0] * 9  # 8 byte register
 
-        # register holds 8 bytes
-        self.reg = [0] * 8
-        # 256 bytes of memory
-        self.ram = [0] * 256
-        # Program Counter, index into ram 
-        self.pc =  0
+        self.pc = 0    # program counter
+        self.reg[7] = self.ram[0xF4]   # set default f4 in register 7
+        self.sp = self.reg[7]  # sp == stackpointer
 
-    #  accept the address to read and return the value stored there
-    def ram_read(self, address):
-        return self.ram[address]
+        self.dispatch_table = {}
+        self.dispatch_table[LDI] = self.ldi
+        self.dispatch_table[PRN] = self.prn
+        self.dispatch_table[MUL] = self.mul
+        self.dispatch_table[PUSH] = self.push
+        self.dispatch_table[POP] = self.pop
+        self.dispatch_table[CALL] = self.call
+        self.dispatch_table[RET] = self.ret
+        self.dispatch_table[ADD] = self.add
 
-    # accept a value to write, and the address to write it to 
-    def ram_write(self, address, value):
-        self.ram[address] = value
+        # Sprint challenge: add default flag, CMP, JMP, JEQ and JNE
+        self.fl = 0b00000000  # default flag
 
-    # Load a value in register
-    def LDI(self, a, value):
-        self.reg[a] = value
+        self.dispatch_table[CMP] = self.cmp
+        self.dispatch_table[JMP] = self.jmp
+        self.dispatch_table[JEQ] = self.jeq
+        self.dispatch_table[JNE] = self.jne
 
-    # Print value at register index  
-    def PRN(self, a):
-        print(self.reg[a])
 
-    # Halt the CPU
-    def HLT(self):
-        sys.exit()
+    def ram_read(self, pc):
+        print(self.ram[pc])
 
-    def POP(self, a):
-        addr_to_pop_from = self.reg[7]
-        value = self.ram_read(addr_to_pop_from)
+    def ram_write(self, value):
+        self.ram.append(value)
 
-        reg_num = self.ram_read(self.pc + 1)
-        self.reg[reg_num] = value
-        
-        self.reg[7] += 1
+    def ldi(self):
+        register = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
 
-    def PUSH(self, reg):
-        self.reg[7] -= 1
+        self.pc += 3
+        self.reg[register] = value
 
-        reg_num = self.ram_read(self.pc + 1)
-        value = self.reg[reg_num]
+    def prn(self):
+        register = self.ram[self.pc + 1]
+        print(self.reg[register])
+        self.pc += 2
 
-        addr_to_push_to = self.reg[7]
-        self.ram_write(value, addr_to_push_to)
+    def mul(self):
+        register1 = self.ram[self.pc + 1]
+        register2 = self.ram[self.pc + 2]
+        self.alu("MUL", register1, register2)
+        self.pc += 3
 
-    def MUL(self, a, b):
-        self.alu('MUL', a, b)
-    
-    def ADD(self, regA, regB):
-        self.alu('ADD', regA, regB)
+    def add(self):
+        register1 = self.ram[self.pc + 1]
+        register2 = self.ram[self.pc + 2]
+        self.alu("ADD", register1, register2)
+        self.pc += 3
 
-    def CALL(self, reg): 
-        return_addr = self.pc + 2
+    def push(self):
+        self.sp -= 1
+        register = self.ram[self.pc + 1]
+        self.ram[self.sp] = self.reg[register]
+        self.pc += 2
 
-        self.reg[7] -= 1
-        addr_to_push_to = self.reg[7]
-        self.ram_write(return_addr,addr_to_push_to)
+    def pop(self):
+        register = self.ram[self.pc + 1]
+        self.reg[register] = self.ram[self.sp]
+        self.sp += 1
+        self.pc += 2
 
-        reg_num = self.ram_read(self.pc + 1)
-        subroutine_addr = self.reg[reg_num]
+    def call(self):
+        self.sp -= 1
+        return_address = self.pc + 2
+        self.ram[self.sp] = return_address
 
-        self.pc = subroutine_addr
+        register = self.ram[self.pc + 1]
+        self.pc = self.reg[register]
 
-    def RET(self):
-        addr_to_pop_from = self.reg[7]
-        return_addr = self.ram_read(addr_to_pop_from)
-        self.reg[7] += 1
+    def ret(self):
+        self.pc = self.ram[self.sp]
+        self.sp += 1
 
-        self.pc = return_addr
+    # *************************************************
+    # Sprint Challenge methods
+    # *************************************************
+
+    def jne(self):
+        register = self.ram[self.pc + 1]
+        if (self.fl & HLT) == 0:
+            self.pc = self.reg[register]
+        else:
+            self.pc += 2
+
+    def jeq(self):
+        register = self.ram[self.pc + 1]
+        if (self.fl & HLT) > 0:
+            self.pc = self.reg[register]
+        else:
+            self.pc += 2
+
+    def cmp(self):
+        register1 = self.ram[self.pc + 1]
+        register2 = self.ram[self.pc + 2]
+        self.alu("CMP", register1, register2)
+        self.pc += 3
+
+    def jmp(self):
+        register = self.ram[self.pc + 1]
+        self.pc = self.reg[register]
+
+    # **********************************************
 
     def load(self):
         """Load a program into memory."""
-        
-        # At address 0...(memory controller accesses RAM via address)
-        address = 0
 
-        if len(sys.argv) != 2:
-            print("usage: cpu.py ")
-            sys.exit(1)
-        try:
-            with open(sys.argv[1]) as filename:
-                for line in filename:
-                    try:
-                        line = line.split("#",1)[0]
-                        line = int(line, 2)  # int() is base 10 by default
-                        self.ram[address] = line
-                        address += 1
-                    except ValueError:
-                        pass
-        except FileNotFoundError:
-            print(f"Couldn't find file {sys.argv[1]}")
-        
+        address = 0
+        filename = sys.argv[1]
+        program = []
+
+        with open(filename) as f:
+            for line in f:
+                n = line.split('#')
+                n[0] = n[0].strip()
+
+                if n[0] == '':
+                    continue
+                val = int(n[0], 2)
+                program.append(val)
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
         elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
-        else: 
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+
+        elif op == "CMP":
+            register_a = self.reg[reg_a]
+            register_b = self.reg[reg_b]
+            if register_a == register_b:
+                self.fl = 0b00000001
+            elif register_a > register_b:
+                self.fl = 0b00000010
+            elif register_a < register_b:
+                self.fl = 0b00000100
+
+        else:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
@@ -116,8 +182,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -126,51 +192,14 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
+        print()
 
     def run(self):
         """Run the CPU."""
         running = True
 
-        dispatch_table = {
-            '0b10000010': self.LDI,
-            '0b01000111': self.PRN,
-            '0b00000001': self.HLT,
-            '0b01000101': self.PUSH,
-            '0b01000110': self.POP,
-            '0b10100010': self.MUL,
-            '0b10100000': self.ADD,
-            '0b01010000': self.CALL, 
-            '0b00010001': self.RET
-        }
-        
         while running:
-            ir = self.ram_read(self.pc)
-            # operand 1
-            op1 = self.ram_read(self.pc+1)
-            # operand 2
-            op2 = self.ram_read(self.pc+2)
-
-            binStr = format(ir, '#010b')
-            args = binStr[2:4]
-            alu_op = binStr[4]
-            set_pc = binStr[5]
-            inst_id = binStr[6:]
-
-            if binStr in dispatch_table:
-                function = dispatch_table[binStr]
-                if args == "00": 
-                    function()
-                    if set_pc == '0': 
-                        self.pc += 1
-                elif args == "01": 
-                    function(op1)
-                    if set_pc == '0': 
-                        self.pc += 2
-                elif args == "10": 
-                    function(op1, op2)
-                    if set_pc == '0':
-                        self.pc += 3
-
-            # else:
-            #     print(f"Did not understand that instruction: {ir} at address {self.pc}")
-            #     sys.exit(1)
+            if self.ram[self.pc] == HLT:
+                running = False
+            else:
+                self.dispatch_table[self.ram[self.pc]]()
